8d0a95ecc66590e6ea5df599a0fa759b7e10e25e
zickmatthias <zick.matthias95@gmail.com>
2024-12-16 21:01:37 +0100

    .

---
+++ data.js
@@ -0,0 +1,102 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20240926_110930_00_136_hdr",
+      "name": "IMG_20240926_110930_00_136_HDR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.027066172535969102,
+        "pitch": 0.054340865120513726,
+        "fov": 1.5687854873633549
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0473945584723614,
+          "pitch": -0.004838567637518665,
+          "rotation": 0,
+          "target": "1-img2_20240926_103953_00_127-2_hdr"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7442471628975582,
+          "pitch": 0.119319839448087,
+          "title": "Küche",
+          "text": "Zum zubereiten leckerer Sommerrollen"
+        }
+      ]
+    },
+    {
+      "id": "1-img2_20240926_103953_00_127-2_hdr",
+      "name": "IMG2_20240926_103953_00_127 2_HDR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.2829196926928432,
+        "pitch": 0.20159669973936012,
+        "fov": 1.5687854873633549
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4071765625052084,
+          "pitch": 0.030226033169286737,
+          "rotation": 6.283185307179586,
+          "target": "0-img_20240926_110930_00_136_hdr"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Probetour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
